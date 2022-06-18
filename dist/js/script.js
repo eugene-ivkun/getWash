31d0e3631184548bdcffb6403bd537965479fbc6
eugene ivkun <2ivkun2@gmail.com>
2022-06-18 17:13:03 +0300

    last-prod

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,21 +1,10 @@
 $(document).click(function(e) {
-    let btn = $(e.target).closest("[data-open-class]");
-    let elemClass = btn.attr("data-open-class");
+    let btn = $(e.target).closest("[data-toggle-class]");
+    let elemClass = btn.attr("data-toggle-class");
     if(!elemClass) return;
     let elemMass = elemClass.split(",");
     $.each(elemMass, function(index, el) {
         $("." + el).toggleClass(el + "--open");
-    });
-    let btnClass = btn.attr("data-btn-class");
-    if(!btnClass) return;
-    let btnElem = $("." + btnClass);
-    btnElem.toggleClass(btnClass + "--open");
-});
-
-$(document).ready(function() {
-    $(".reviews__slider-inner").slick({
-        nextArrow: $(".reviews__slider-btn--next"),
-        prevArrow: $(".reviews__slider-btn--prev")
     });
 });
 
@@ -24,8 +13,16 @@ $(document).click(function(e) {
     let scrollClass = btn.attr("data-scrollto");
     if(!scrollClass) return;
     let scrollElem = $(scrollClass);
-    // scrollTop( scrollElem.offset().top - 50 )
     $("html, body").animate({
         scrollTop:  scrollElem.offset().top - 50
     }, 1000);
+});
+
+
+
+$(document).ready(function() {
+    $(".reviews__slider-inner").slick({
+        nextArrow: $(".reviews__slider-btn--next"),
+        prevArrow: $(".reviews__slider-btn--prev")
+    });
 });
