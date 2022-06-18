ed7733927451e427f27629f4aa76717523ca8a9f
eugene ivkun <2ivkun2@gmail.com>
2022-06-18 21:13:20 +0300

    fimal project

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,25 +1,17 @@
-// $(document).click(function(e) {
-//     let btn = $(e.target).closest("[data-toggle-class]");
-//     let elemClass = btn.attr("data-toggle-class");
-//     if(!elemClass) return;
-//     let elemMass = elemClass.split(",");
-//     $.each(elemMass, function(index, el) {
-//         let elem = $("." + el);
-//         elem.toggleClass(el + "--open");
-//     });
-// });
+$(".js-menu-btn").click(function() {
+    $(".menu-btn").toggleClass("active");
+    $(".nav").toggleClass("active");
+    $(".header__line").toggleClass("active")
+});
 
-// $(document).click(function(e) {
-//     let btn = $(e.target).closest("[data-scrollto]");
-//     let scrollClass = btn.attr("data-scrollto");
-//     if(!scrollClass) return;
-//     let scrollElem = $(scrollClass);
-//     $("html, body").animate({
-//         scrollTop:  scrollElem.offset().top - 50
-//     }, 1000);
-// });
-
-
+$("[data-scrollto]").click(function() {
+    $(".menu-btn").removeClass("active");
+    $(".nav").removeClass("active");
+    $(".header__line").removeClass("active");
+    $("html, body").animate({
+        scrollTop: $($(this).attr("data-scrollto")).offset().top - 50
+    }, 1000);
+});
 
 $(document).ready(function() {
     $(".reviews__slider-inner").slick({
